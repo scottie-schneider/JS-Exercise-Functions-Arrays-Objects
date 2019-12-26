97cdbb10c6c5c09ed22b4b1d3afec67a5166e5e5
scottie-schneider <scott@laclave.co>
2019-12-26 15:22:13 -0500

    all tests passing + stretch

--- index.js
+++ index.js
@@ -323,8 +323,16 @@ const argTimesTwo = (num) => num * 2; // code here!
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
+function carMaker(miles) {
   /* code here */
+  const car = {
+    odometer: miles,
+    drive(distance){
+      this.odometer = this.odometer + distance
+      return this.odometer
+    }
+  }
+  return car
 }
 
 /// ////// END OF CHALLENGE /////////
